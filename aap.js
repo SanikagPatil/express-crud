b29c10a130e0cabde98b018cc63afbf2a2fd1894
SanikagPatil <sanikagpatil12@gmail.com>
2025-02-08 14:30:14 +0530

    Add files via upload

---
+++ aap.js
@@ -0,0 +1,19 @@
+//function display()
+//{
+  //  return display()
+//}
+//Module.exports=display
+//function display(a,b)
+//{
+  //  return a+b
+//}
+//module.exports=display
+function add(a,b)
+{
+    return a+b
+}
+   function sub(a,b)
+   {
+    return a-b
+   }
+   module.exports={add,sub}
